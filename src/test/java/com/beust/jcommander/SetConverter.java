f2430d09b28ed192a4e0f637e264a19d7604d8ba
Cedric Beust <cedric@beust.com>
2011-08-17 14:10:51 -0700

    Forgot a class.

---
+++ src/test/java/com/beust/jcommander/SetConverter.java
@@ -0,0 +1,16 @@
+package com.beust.jcommander;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class SetConverter implements IStringConverter<SortedSet<Integer>> {
+ 
+  public SortedSet<Integer> convert(String value) {
+    SortedSet<Integer> set = new TreeSet<Integer>();
+    String[] values = value.split(",");
+    for (String num : values) {
+      set.add(Integer.parseInt(num));
+    }
+    return set;
+  }
+}
