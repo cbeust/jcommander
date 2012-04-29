c84dbd38b26b5713a776745140881c6e0b7c7aac
Cedric Beust <cedric@beust.com>
2012-04-29 15:17:05 -0700

    Fixed: variable arities not working when same parameter appears multiple times.

--- src/main/java/com/beust/jcommander/JCommander.java
+++ src/main/java/com/beust/jcommander/JCommander.java
@@ -610,7 +610,7 @@ public class JCommander {
               //
               // Variable arity?
               //
-              i += processVariableArity(args, i, pd);
+              i = processVariableArity(args, i, pd);
             } else {
               //
               // Regular option
--- src/test/java/com/beust/jcommander/JCommanderTest.java
+++ src/test/java/com/beust/jcommander/JCommanderTest.java
@@ -701,11 +701,21 @@ public class JCommanderTest {
       private List<String> paramB = Lists.newArrayList();
     }
 
-    String args[] = { "-paramA", "a1", "a2", "-paramB", "b1", "b2", "b3" };
-    Params p = new Params();
-    new JCommander(p, args).parse();
-    Assert.assertEquals(p.paramA, Arrays.asList(new String[] { "a1", "a2" }));
-    Assert.assertEquals(p.paramB, Arrays.asList(new String[] { "b1", "b2", "b3" }));
+    {
+      String args[] = { "-paramA", "a1", "a2", "-paramB", "b1", "b2", "b3" };
+      Params p = new Params();
+      new JCommander(p, args).parse();
+      Assert.assertEquals(p.paramA, Arrays.asList(new String[] { "a1", "a2" }));
+      Assert.assertEquals(p.paramB, Arrays.asList(new String[] { "b1", "b2", "b3" }));
+    }
+
+    {
+      String args[] = { "-paramA", "a1", "a2", "-paramB", "b1", "-paramA", "a3" };
+      Params p = new Params();
+      new JCommander(p, args).parse();
+      Assert.assertEquals(p.paramA, Arrays.asList(new String[] { "a1", "a2", "a3" }));
+      Assert.assertEquals(p.paramB, Arrays.asList(new String[] { "b1" }));
+    }
   }
 
   @Test(enabled = false,
@@ -724,7 +734,7 @@ public class JCommanderTest {
 
   @Test(enabled = false)
   public static void main(String[] args) throws Exception {
-    new JCommanderTest().commandKey();
+    new JCommanderTest().multiVariableArityList();
 //    System.out.println("Help:" + a.help);
 //    System.out.println("A");
 //    class A {
