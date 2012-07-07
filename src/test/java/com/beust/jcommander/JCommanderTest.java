1b0c674e93eb5d4a34607797783d163800e73a1e
Cedric Beust <cedric@beust.com>
2012-07-07 08:57:53 -0700

    Moved the method tests in their own class.

--- src/test/java/com/beust/jcommander/JCommanderTest.java
+++ src/test/java/com/beust/jcommander/JCommanderTest.java
@@ -242,38 +242,6 @@ public class JCommanderTest {
     Assert.assertEquals(args.rest.get(0), "rest");
   }
 
-  public void arityStringsSetter() {
-    class ArgsArityStringSetter {
-
-      @Parameter(names = "-pairs", arity = 2, description = "Pairs")
-      public void setPairs(List<String> pairs) {
-        this.pairs = pairs;
-      }
-      public List<String> getPairs() {
-        return this.pairs;
-      }
-      public List<String> pairs;
-
-      @Parameter(description = "Rest")
-      public void setRest(List<String> rest) {
-        this.rest = rest;
-      }
-      public List<String> getRest() {
-        return this.rest;
-      }
-      public List<String> rest;
-    }
-    ArgsArityStringSetter args = new ArgsArityStringSetter();
-    String[] argv = { "-pairs", "pair0", "pair1", "rest" };
-    new JCommander(args, argv);
-
-    Assert.assertEquals(args.pairs.size(), 2);
-    Assert.assertEquals(args.pairs.get(0), "pair0");
-    Assert.assertEquals(args.pairs.get(1), "pair1");
-    Assert.assertEquals(args.rest.size(), 1);
-    Assert.assertEquals(args.rest.get(0), "rest");
-  }
-
   @Test(expectedExceptions = ParameterException.class)
   public void arity2Fail() {
     ArgsArityString args = new ArgsArityString();
@@ -837,26 +805,9 @@ public class JCommanderTest {
     Assert.assertTrue(arg.help);
   }
 
-  public void setterThatThrows() {
-    class Arg {
-      @Parameter(names = "--host")
-      public void setHost(String host) {
-        throw new ParameterException("Illegal host");
-      }
-    }
-    boolean passed = false;
-    try {
-      new JCommander(new Arg(), new String[] { "--host", "host" });
-    } catch(ParameterException ex) {
-      Assert.assertEquals(ex.getCause(), null);
-      passed = true;
-    }
-    Assert.assertTrue(passed, "Should have thrown an exception");
-  }
-
   @Test(enabled = false)
   public static void main(String[] args) throws Exception {
-    new JCommanderTest().setterThatThrows();
+    new JCommanderTest().verifyHelp();
 //    class A {
 //      @Parameter(names = "-short", required = true)
 //      List<String> parameters;
