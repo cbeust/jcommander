75d579a2fc672582cfadefc360fe18b05caa7e21
Cedric Beust <cedric@beust.com>
2012-07-08 10:37:23 -0700

    Forgot Nullable.java

---
+++ src/main/java/com/beust/jcommander/internal/Nullable.java
@@ -0,0 +1,12 @@
+package com.beust.jcommander.internal;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target({FIELD, PARAMETER})
+public @interface Nullable {
+}
