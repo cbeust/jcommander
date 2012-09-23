5fc805c44e904eb37f24217676b5da1619268c48
Cedric Beust <cedric@beust.com>
2012-09-23 06:10:45 -0700

    Added enum support (Scott M. Stark).

--- src/main/java/com/beust/jcommander/JCommander.java
+++ src/main/java/com/beust/jcommander/JCommander.java
@@ -1235,6 +1235,10 @@ public class JCommander {
       converterClass = elementType != null
           ? findConverter((Class<? extends IStringConverter<?>>) elementType)
           : StringConverter.class;
+      // Check for enum type parameter
+      if (converterClass == null && Enum.class.isAssignableFrom((Class) elementType)) {
+        converterClass = (Class<? extends IStringConverter<?>>) elementType;
+      }
     }
 
     //
@@ -1257,7 +1261,7 @@ public class JCommander {
     try {
       String[] names = annotation.names();
       String optionName = names.length > 0 ? names[0] : "[Main class]";
-      if (converterClass.isEnum()) {
+      if (converterClass != null && converterClass.isEnum()) {
         try {
           result = Enum.valueOf((Class<? extends Enum>) converterClass, value.toUpperCase());
         } catch (Exception e) {
--- src/test/java/com/beust/jcommander/args/ArgsEnum.java
+++ src/test/java/com/beust/jcommander/args/ArgsEnum.java
@@ -18,12 +18,14 @@
 
 package com.beust.jcommander.args;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
 
 import org.testng.Assert;
 
-import java.util.EnumSet;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 
 /**
  * Test enums.
@@ -36,6 +38,9 @@ public class ArgsEnum {
   @Parameter(names = "-choice")
   public ChoiceType choice = ChoiceType.ONE;
   
+  @Parameter(names = "-choices", variableArity = true)
+  public List<ChoiceType> choices = new ArrayList<ChoiceType>();
+
   public static void main(String[] args1) {
     ArgsEnum args = new ArgsEnum();
     String[] argv = { "-choice", "ONE"};
