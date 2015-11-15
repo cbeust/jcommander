39c557de9a2dadb7375ec9f41d0c535e668aadf9
Cedric Beust <cedric@beust.com>
2015-11-15 13:20:19 -0800

    Don't need gson.

--- Build.kt
+++ Build.kt
@@ -9,14 +9,9 @@ val jcommander = javaProject {
     artifactId = name
     version = "1.54"
 
-    dependencies {
-        compile("com.google.code.gson:gson:2.4")
-    }
-
     dependenciesTest {
         compile("org.testng:testng:6.9.9")
     }
-
 
     assemble {
         mavenJars {
