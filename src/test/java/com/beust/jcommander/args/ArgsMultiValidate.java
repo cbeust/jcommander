7f8a496bfb16e838c8f7e571c09f3559eede011c
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2017-03-25 15:34:22 +0100

    typo

--- src/test/java/com/beust/jcommander/args/ArgsMultiValidate.java
+++ src/test/java/com/beust/jcommander/args/ArgsMultiValidate.java
@@ -14,23 +14,22 @@ public class ArgsMultiValidate {
 	}
   }
 
-  public static class LowerThan100VvalueValidator implements IValueValidator<Integer> {
+  public static class LowerThan100ValueValidator implements IValueValidator<Integer> {
 	 @Override
 	public void validate(String name, Integer value) throws ParameterException {
 			if(value>=100) throw new ParameterException("param "+name+"="+value+" is greater than 100");
 	 }
   } 
   
-  public static class GreaterTha0VvalueValidator implements IValueValidator<Integer> {
+  public static class GreaterTha0ValueValidator implements IValueValidator<Integer> {
 		 @Override
 		public void validate(String name, Integer value) throws ParameterException {
-			 System.err.println(name+"="+value+"######################");
 			 if(value<=0) throw new ParameterException("param "+name+"="+value+" is lower than 1");
 		 }
 	  }
   
   @Parameter(names = "-age",
 		  validateWith = {PositiveInteger.class,OddIntegerParameterValidator.class},
-		  validateValueWith={GreaterTha0VvalueValidator.class,LowerThan100VvalueValidator.class})
+		  validateValueWith={GreaterTha0ValueValidator.class,LowerThan100ValueValidator.class})
   public int age=29;
 }
