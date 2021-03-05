b4b2c05ff8ecf8c0786cc297f5b301af150c5bbf
Tim Gallagher aka Tim Fegan <timgallagheractor@gmail.com>
2021-03-04 22:16:07 -0700

    Update IParameterizedParser.java
    Added more comments on why this interface was created.

--- src/main/java/com/beust/jcommander/IParameterizedParser.java
+++ src/main/java/com/beust/jcommander/IParameterizedParser.java
@@ -3,6 +3,10 @@ package com.beust.jcommander;
 import java.util.List;
 
 /**
+ * Thin interface allows the Parameterized parsing mechanism, which reflects an object to find the 
+ * JCommander annotations, to be replaced at runtime for cases where the source code can not
+ * be directly annotated with JCommander annotations, but may have other annotations such as 
+ * Json annotations that can be used to reflect as JCommander parameters.
  *
  * @author Tim Gallagher
  */
@@ -12,9 +16,9 @@ public interface IParameterizedParser {
    * Parses the given object for any command line related annotations and returns the list of 
    * JCommander Parameterized definitions.
    * 
-   * @param arg the object that contains the annotations.
+   * @param annotatedObj the object that contains the annotations.
    * @return non-null List but may be empty
    */
-  List<Parameterized> parseArg(Object arg);
+  List<Parameterized> parseArg(Object annotatedObj);
   
 }
