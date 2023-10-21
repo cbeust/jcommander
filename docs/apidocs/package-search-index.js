c39085f6a1c2b987856d777796fe255a6c2d8c2c
Markus KARG <markus@headcrashing.eu>
2023-10-21 13:50:47 +0000

    Creating apidocs using JavaDoc when documentation is created

---
+++ docs/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.beust.jcommander"},{"l":"com.beust.jcommander.converters"},{"l":"com.beust.jcommander.defaultprovider"},{"l":"com.beust.jcommander.internal"},{"l":"com.beust.jcommander.parser"},{"l":"com.beust.jcommander.validators"}]
--- docs/generate-doc.sh
+++ docs/generate-doc.sh
@@ -1,1 +1,4 @@
 asciidoctor index.adoc -o index.html -a stylesheet=golo.css
+pushd ..
+./gradlew javadoc && mkdir -p docs/apidocs && cp -r build/docs/javadoc/* docs/apidocs
+popd
