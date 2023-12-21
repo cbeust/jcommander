46b12bd4627286db8ee6d38fc6adc71232c85bf3
Jan de Gorter <Jan.de.Gorter@rabobank.nl>
2023-12-21 14:05:07 +0000

    Asserting that double quotes are not removed.

---
+++ src/test/java/com/beust/jcommander/TestParameterQuoteHandling.java
@@ -0,0 +1,35 @@
+package com.beust.jcommander;
+
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Parameters(separators = "=", commandDescription = "Just for testing quote handling.")
+public class TestParameterQuoteHandling {
+
+    @Parameter(names = { "--aParameter" }, description = "A String.")
+    public String aParameter = null;
+
+    @Parameter(names = { "--aParameterList"}, description = "A String list.")
+    public List<String> aParameterList = null;
+
+    @Test
+    public void testParameterHavingQuotes() {
+        JCommander jc = new JCommander(this);
+        jc.parse("--aParameter=\"X\"");
+        Assert.assertNotNull(aParameter);
+        // as of JCommander 1.74/75
+        Assert.assertEquals(aParameter, "\"X\"", "Expect \"X\" for JCommander 1.74/75 and more recent");
+    }
+
+    @Test
+    public void testParameterListHavingQuotes() {
+        JCommander jc = new JCommander(this);
+        jc.parse("--aParameterList=\"X,Y\"");
+        Assert.assertNotNull(aParameterList);
+        Assert.assertEquals(aParameterList.size(), 2);
+        Assert.assertEquals(aParameterList.get(0), "\"X");
+        Assert.assertEquals(aParameterList.get(1), "Y\"");
+    }
+}
