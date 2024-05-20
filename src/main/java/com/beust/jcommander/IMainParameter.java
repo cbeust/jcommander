22dd87acbd7f32051425f448cbef2794e6c92b63
Jaume Ribas <r.ribas.jaume@gmail.com>
2024-05-20 13:50:13 +0200

    Changed copyright year to 2024 in IMainParameter.java

--- src/main/java/com/beust/jcommander/IMainParameter.java
+++ src/main/java/com/beust/jcommander/IMainParameter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010 the original author or authors.
+ * Copyright (C) 2024 the original author or authors.
  * See the notice.md file distributed with this work for additional
  * information regarding copyright ownership.
  *
