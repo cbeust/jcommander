fcf0795794d1fe0aecc29fc852f764c0668e93df
Markus KARG <markus@headcrashing.eu>
2024-12-23 17:10:17 +0000

    Using records

--- src/main/java/com/beust/jcommander/StringKey.java
+++ src/main/java/com/beust/jcommander/StringKey.java
@@ -2,13 +2,7 @@ package com.beust.jcommander;
 
 import com.beust.jcommander.FuzzyMap.IKey;
 
-public class StringKey implements IKey {
-
-  private String name;
-
-  public StringKey(String name) {
-    this.name = name;
-  }
+public record StringKey(String name) implements IKey {
 
   @Override
   public String getName() {
@@ -18,31 +12,6 @@ public class StringKey implements IKey {
   @Override
   public String toString() {
     return name;
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((name == null) ? 0 : name.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    StringKey other = (StringKey) obj;
-    if (name == null) {
-      if (other.name != null)
-        return false;
-    } else if (!name.equals(other.name))
-      return false;
-    return true;
   }
 
 }
